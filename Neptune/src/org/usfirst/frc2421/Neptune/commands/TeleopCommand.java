578403be5aedc8ba48e949c1f8816da5885d7d10
Raghav Goyal <raghavgoyal2006@hotmail.com>
2013-02-10 23:52:29 -0500

    Clay Sweetser - Commented code, added TODO's , as well as a file highlighting what should be done to clean up code.

---
+++ Neptune/src/org/usfirst/frc2421/Neptune/commands/TeleopCommand.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.usfirst.frc2421.Neptune.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import org.usfirst.frc2421.Neptune.Robot;
+
+/**
+ *
+ * @author Driver
+ */
+public class TeleopCommand extends Command {
+
+    boolean finished;
+
+    public TeleopCommand() {
+        finished = false;
+    }
+
+    protected void initialize() {
+    }
+
+    protected void execute() {
+        if (!finished) {
+            Scheduler.getInstance().add(new TeleDrive());
+            finished = true;
+        }
+    }
+
+    protected boolean isFinished() {
+        return finished;
+    }
+
+    protected void end() {
+    }
+
+    protected void interrupted() {
+    }
+}
