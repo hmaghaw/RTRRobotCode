d2101b804252be966902f03092bfc9ef0139957f
kalzilla757 <kalzilla757@gmail.com>
2013-02-17 16:59:59 -0500

    Remains

--- Neptune/src/org/usfirst/frc2421/Neptune/commands/loader/loadFrisbee.java
+++ Neptune/src/org/usfirst/frc2421/Neptune/commands/loader/loadFrisbee.java
@@ -23,55 +23,38 @@ public class loadFrisbee extends Command {
         hasLoaded = false;
         while (!Robot.loaderSystem.getRestSwitch()) { // Add limit/timeout
             Robot.loaderSystem.startLoaderArm(.5);
-            isForward = true;
-            end = false;
         }
     }
 
     // Called repeatedly when this Command is scheduled to run
     public void execute() {
 
-        if (isForward && !Robot.loaderSystem.getFiredSwitch()) {
-            Robot.loaderSystem.startLoaderArm(-.5);
-        }
-        if (Robot.loaderSystem.getFiredSwitch() && isForward) {
-            Robot.loaderSystem.stopMotor();
-            isForward = false;
-        }
-        if (Robot.loaderSystem.getFiredSwitch() && !isForward) {
+        if (!hasLoaded && !Robot.loaderSystem.getFiredSwitch()) {
             Robot.loaderSystem.startLoaderArm(.5);
-            hasLoaded = false;
-            
+        } else if (Robot.loaderSystem.getFiredSwitch() && !hasLoaded) {
+            Robot.loaderSystem.startLoaderArm(-.5);
+            hasLoaded = true;
+
         } else if (Robot.loaderSystem.getRestSwitch() && hasLoaded) {
             Robot.loaderSystem.stopArm();
             finished = true;
         }
-//        if (hasLoaded && !Robot.loaderSystem.getFiredSwitch()) {
-//            Robot.loaderSystem.startLoaderArm(-.5);
-//        }
-//        if (Robot.loaderSystem.getFiredSwitch() && hasLoaded) {
-//            Robot.loaderSystem.stopArm();
-//            hasLoaded = false;
-//        }
-//        if (Robot.loaderSystem.getFiredSwitch() && !hasLoaded) {
-//            Robot.loaderSystem.startLoaderArm(.5);
-//        }
-//        if (Robot.loaderSystem.getRestSwitch() && !hasLoaded) {
-//            Robot.loaderSystem.stopArm();        
-//            end = true;
     }
-}
-protected boolean isFinished() {
+
+    protected boolean isFinished() {
         return finished;
     }
 
     // Called once after isFinished returns true
     protected void end() {
-        Robot.loaderSystem.stopMotor();
+        Robot.loaderSystem.stopArm();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+        while (!Robot.loaderSystem.getRestSwitch()) { // Add limit/timeout
+            Robot.loaderSystem.startLoaderArm(.5);
+        }
     }
 }
