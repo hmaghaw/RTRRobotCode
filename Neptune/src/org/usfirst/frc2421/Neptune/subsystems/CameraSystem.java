24d157f6f3c4cc286491728fba0500949664c41c
Varriount <clay.sweetser@gmail.com>
2013-03-28 13:06:08 -0400

    Finally updated

--- Neptune/src/org/usfirst/frc2421/Neptune/subsystems/CameraSystem.java
+++ Neptune/src/org/usfirst/frc2421/Neptune/subsystems/CameraSystem.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.camera.AxisCamera;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.image.CriteriaCollection;
 import edu.wpi.first.wpilibj.image.NIVision.MeasurementType;
+import edu.wpi.first.wpilibj.image.ParticleAnalysisReport;
 import edu.wpi.first.wpilibj.tables.ITable;
 import org.usfirst.frc2421.Neptune.RobotMap;
 import org.usfirst.frc2421.Neptune.utils.RobotUtils;
@@ -19,6 +20,11 @@ public class CameraSystem extends Subsystem {
     public final CriteriaCollection cc;
     public Scores latestScores;
     public Relay lightRelay;
+    public ParticleAnalysisReport latestReport;
+    
+    public int goalnumber;
+    public String goalString;
+    
 
     public CameraSystem() {
         camera = RobotMap.camera;  // get an instance of the camera
