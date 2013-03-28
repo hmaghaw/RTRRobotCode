10ae4e01cf33d36e47317de316f1f825990c2941
Varriount <clay.sweetser@gmail.com>
2013-03-28 12:51:21 -0400

    updating to match Karl'sHouse branch

--- Neptune/src/org/usfirst/frc2421/Neptune/commands/shoot/AngleManipulation.java
+++ Neptune/src/org/usfirst/frc2421/Neptune/commands/shoot/AngleManipulation.java
@@ -8,58 +8,68 @@ import org.usfirst.frc2421.Neptune.Robot;
  */
 public class AngleManipulation extends Command {
 
+    private double angle;
+    private double joystickValue;
+    private double lowerLimit, upperLimit;
+    private boolean end;
+
     public AngleManipulation() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);	
         requires(Robot.shootSystem);
     }
-    private double angle;
-    private double joystickValue;
+
     // Called just before this Command runs the first time
     protected void initialize() {
         Robot.shootSystem.angle = Robot.shootSystem.measureAngleOfFire.getAverageValue() * 72;
+        lowerLimit = Robot.shootSystem.lowerLimit;
+        upperLimit = Robot.shootSystem.upperLimit;
+        end = false;
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
         /*double currentAngle = Robot.shooter.checkGyroAngle();
-        double targetAngle = 0;
-        double angleSeparation = targetAngle - currentAngle;
-        double acceptableDifference = .75;
-        if(angleSeparation > acceptableDifference){
-            try {
-                Robot.shooter.shooterAngleIncrease();
-            } catch (Exception ex) {
-            }
-            }
-       else if(angleSeparation < acceptableDifference){
-            try {
-                Robot.shooter.shooterAngleDecrease();
-            } catch (CANTimeoutException ex) {
-            }
-       }
-       else{
-            try {
-                Robot.shooter.stopAngleMotor();
-            } catch (Exception ex) {
-            }
-       }*/
-       //Robot.shootSystem.angle = Robot.shootSystem.measureAngleOfFire.getAverageValue() * 72;
+         double targetAngle = 0;
+         double angleSeparation = targetAngle - currentAngle;
+         double acceptableDifference = .75;
+         if(angleSeparation > acceptableDifference){
+         try {
+         Robot.shooter.shooterAngleIncrease();
+         } catch (Exception ex) {
+         }
+         }
+         else if(angleSeparation < acceptableDifference){
+         try {
+         Robot.shooter.shooterAngleDecrease();
+         } catch (CANTimeoutException ex) {
+         }
+         }
+         else{
+         try {
+         Robot.shooter.stopAngleMotor();
+         } catch (Exception ex) {
+         }
+         }*/
+        //Robot.shootSystem.angle = Robot.shootSystem.measureAngleOfFire.getAverageValue() * 72;
         joystickValue = Robot.oi.shooterStick.getY();
-        
-        if(joystickValue > .2){
-            Robot.shootSystem.shooterAngleIncrease(joystickValue);
-        }
-        else if(joystickValue < -.2){
-            Robot.shootSystem.shooterAngleDecrease(joystickValue);
-        }
-        else{
+        if (angle > lowerLimit && angle < upperLimit) {
+            if (joystickValue > .2) {
+                Robot.shootSystem.shooterAngleIncrease(joystickValue);
+            } else if (joystickValue < -.2) {
+                Robot.shootSystem.shooterAngleDecrease(joystickValue);
+            } else {
+                Robot.shootSystem.stopAngleMotor();
+                end = true;
+            }
+        } else {
             Robot.shootSystem.stopAngleMotor();
+            end = true;
         }
     }
-        
+
     protected boolean isFinished() {
-        return false;
+        return end;
     }
 
     // Called once after isFinished returns true
--- Neptune/src/org/usfirst/frc2421/Neptune/subsystems/ShootSystem.java
+++ Neptune/src/org/usfirst/frc2421/Neptune/subsystems/ShootSystem.java
@@ -21,6 +21,9 @@ public class ShootSystem extends Subsystem {
     public int direction = -1; // wether the motors spin forward (1) or backward (-1)
     public double speedSeparation = .15; //percent the front wheel spins faster than the back wheel 
     public double angle;    //current angle of the shooter Needs to be implemented
+    public double upperLimit = 70;
+    public double lowerLimit = 0;
+    
     //initialize shooter wheels
     public CANJaguar frontWheel = RobotMap.outerShooterWheel;
     public CANJaguar backWheel = RobotMap.innerShooterWheel;
@@ -140,6 +143,11 @@ public class ShootSystem extends Subsystem {
     public double checkCurrentAngle() {
         //determies the angle of the motor.
         return measureAngleOfFire.getAverageVoltage() * 72;
+    }
+    
+    public double getCurrentAngle() {
+        angle = measureAngleOfFire.getAverageVoltage() * 72;
+        return angle;
     }
     
     public void initDefaultCommand() {
